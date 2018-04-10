8b1e5add3d9510d028f191672412d0f0e1b7c1a0
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-10 23:59:40 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,38 +1,55 @@
+# def third_challenge
+#   epic_tragedy = {
+#    :montague => {
+#       :patriarch => {
+#         name: "Lord Montague"#your key/value pairs here
+#         age: "53"
+#         },
+#       :matriarch => {
+#         name: "Lady Montague"#your key/value pairs here
+#         age: "54"
+#         },
+#       :hero => {
+#         name: "Romeo"#your key/value pairs here
+#         age: "15"
+#         status: "alive"
+#         },
+#       :hero_friends => []
+#    },
+#    :capulet => {
+#       :patriarch => {
+#         name: "Lord Capulet"#your key/value pairs here
+#         age: "50"
+#         },
+#       :matriarch => {
+#         name: "Lady Capulet"#your key/value pairs here
+#         age: "51"
+#         },
+#       :heroine => {
+#         name: "Juliet"#your key/value pairs here
+#         age: "15"
+#         status: "alive"
+#         },
+#       :heroine_friends => []
+#    }
+#   }
+# 
+# 
+# end
+
 def third_challenge
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
-      :patriarch => {
-        name: "Lord Montague"#your key/value pairs here
-        age: "53"
-        },
-      :matriarch => {
-        name: "Lady Montague"#your key/value pairs here
-        age: "54"
-        },
-      :hero => {
-        name: "Romeo"#your key/value pairs here
-        age: "15"
-        status: "alive"
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {
-        name: "Lord Capulet"#your key/value pairs here
-        age: "50"
-        },
-      :matriarch => {
-        name: "Lady Capulet"#your key/value pairs here
-        age: "51"
-        },
-      :heroine => {
-        name: "Juliet"#your key/value pairs here
-        age: "15"
-        status: "alive"
-        },
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
-  }
-
-
+}
 end
